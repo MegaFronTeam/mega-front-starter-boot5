af65778815503ddc45ec7c22d45586821f137f90
Pavel <wol1414@gmail.com>
2022-10-03 13:08:46 +0300

    update

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -279,6 +279,29 @@ const JSCCommon = {
 			}
 		}
 	},
+	imgToSVG() {
+    const convertImages = (query, callback) => {
+			const images = document.querySelectorAll(query);
+	
+			images.forEach(image => {
+				fetch(image.src)
+					.then(res => res.text())
+					.then(data => {
+						const parser = new DOMParser();
+						const svg = parser.parseFromString(data, 'image/svg+xml').querySelector('svg');
+	
+						if (image.id) svg.id = image.id;
+						if (image.className) svg.classList = image.classList;
+	
+						image.parentNode.replaceChild(svg, image);
+					})
+					.then(callback)
+					.catch(error => console.error(error))
+			});
+		};
+	
+		convertImages('.img-svg-js');
+  },
 };
 const $ = jQuery;
 
@@ -365,6 +388,7 @@ function eventHandler() {
 		freeModeMomentum: true,
 
 	});
+
 	// modal window
 
 };
