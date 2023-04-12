f33362a0a359e743de317a5adba70f99c5c258f0
Janis <janis.paberzs18@gmail.com>
2023-04-12 11:48:40 +0300

    edit mob menu js

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -62,42 +62,37 @@ const JSCCommon = {
 	},
 	// /modalCall
 	toggleMenu() {
-		document.addEventListener("click", function (event) {
-			const toggle = document.querySelectorAll(".toggle-menu-mobile--js");
-			const menu = document.querySelector(".menu-mobile--js");
-			const toggleEv = event.target.closest(".toggle-menu-mobile--js");
-			if (!toggleEv) return;
-			toggle.forEach(el => el.classList.toggle("on"));
-			menu.classList.toggle("active");
-			[document.body, document.querySelector('html')].forEach(el => el.classList.toggle("fixed"));
-		}, { passive: true });
-	},
-	closeMenu() {
-		const toggle = document.querySelectorAll(".toggle-menu-mobile--js");
-		const menu = document.querySelector(".menu-mobile--js");
-		if (!menu) return;
-		if (menu.classList.contains("active")) {
-			toggle.forEach(element => element.classList.remove("on"));
-			menu.classList.remove("active");
-			[document.body, document.querySelector('html')].forEach(el => el.classList.remove("fixed"));
-		}
+    const toggle = document.querySelectorAll('.toggle-menu-mobile--js');
+    const menu = document.querySelector('.menu-mobile--js');
+    toggle.forEach((el) => el.classList.toggle('on'));
+    menu.classList.toggle('active');
+    [document.body, document.querySelector('html')].forEach((el) => el.classList.toggle('fixed'));
+  },
+  closeMenu() {
+    const toggle = document.querySelectorAll('.toggle-menu-mobile--js');
+    const menu = document.querySelector('.menu-mobile--js');
+    toggle.forEach((element) => element.classList.remove('on'));
+    if (menu) {
+      menu.classList.remove('active');
+      [document.body, document.querySelector('html')].forEach((el) => el.classList.remove('fixed'));
+    }
+  },
+  mobileMenu() {
+    document.addEventListener('click', (event) => {
+        let container = event.target.closest('.menu-mobile--js'); // (1)
+        let toggle = event.target.closest('.toggle-menu-mobile--js'); // (1)
+        if (toggle) this.toggleMenu();
+        if (!container && !toggle) this.closeMenu();
+      },
+      { passive: true },
+    );
 
-	},
-	mobileMenu() { 
-		const menu = document.querySelector(".menu-mobile--js");
-		if (!menu) return;
-		this.toggleMenu();
-		document.addEventListener('mouseup', (event) => {
-			let container = event.target.closest(".menu-mobile--js.active"); // (1)
-			let link = event.target.closest(".menu-mobile .menu a"); // (1)
-			let toggle = event.target.closest('.toggle-menu-mobile--js.on'); // (1)
-			if (!container && !toggle) this.closeMenu();
-		}, { passive: true });
-
-		window.addEventListener('resize', () => {
-			if (window.matchMedia("(min-width: 992px)").matches) this.closeMenu();
-		}, { passive: true });
-	},
+    window.addEventListener('resize', () => {
+        if (window.matchMedia('(min-width: 992px)').matches) this.closeMenu();
+      },
+      { passive: true },
+    );
+  },
 
 	// tabs  .
 	tabscostume(tab) {
